953cfb9ec99ba88f0eb40b474229e12bad15197c
Pilsoo Choi <fpeptm@naver.com>
2017-04-17 15:06:26 +0900

    Commit 3

--- GitHW.java
+++ GitHW.java
@@ -3,6 +3,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Test");
     System.out.println("Hello Test1");
+    System.out.println("Hello Test2");
 
 	}
 
